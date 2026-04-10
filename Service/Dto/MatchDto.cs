5a6ea724c8506c6c6fac1499419af420d42fee52
tamar-goldberg55 <t0548559551@gmail.com>
2026-04-10 04:03:10 +0300

    controler match and listjob

--- Service/Dto/MatchDto.cs
+++ Service/Dto/MatchDto.cs
@@ -14,10 +14,10 @@ namespace Service.Dto
 
         [ForeignKey("JobListings")]
         public int JobId { get; set; } // FK למשרה
-        public JobListings Job { get; set; }
+        public JobListings ?Job { get; set; }
         [ForeignKey("CandidateProfiles")]
         public int CandidateId { get; set; } // FK למועמד
-        public CandidateProfiles Candidate { get; set; }
+        public CandidateProfiles? Candidate { get; set; }
         public double MatchScore { get; set; } // אחוז התאמה (למשל 95.5)
         public DateTime MatchDate { get; set; } // תאריך ריצת האלגוריתם
     }
